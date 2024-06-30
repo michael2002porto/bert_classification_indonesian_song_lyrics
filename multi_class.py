fab669b0f9d1bf077a5a1ffcb4b2020dd0d9b83c
Michael Natanael <mnatanael87@gmail.com>
2024-06-30 14:23:13 +0700

    first commit

---
+++ multi_class.py
@@ -0,0 +1,60 @@
+import argparse
+import sys
+
+from utils.preprocessor_class import PreprocessorClass
+from models.multi_class_model import MultiClassModel
+
+import lightning as L
+
+def collect_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--accelerator", type=str, default="gpu")
+    parser.add_argument("--gpu_id", type=int, default=0)
+    parser.add_argument("--num_nodes", type=int, default=1)
+
+    parser.add_argument("--max_length", type=int,  default=100)
+    parser.add_argument("--batch_size", type=int, default=100)
+    parser.add_argument("--max_epochs", type=int, default=10)
+
+    parser.add_argument("--preprocessed_dir", type=str, default="data/preprocessed")
+    parser.add_argument("--train_data_dir", type=str, default="data/training.res")
+    parser.add_argument("--test_data_dir", type=str, default="data/testing.res")
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = collect_parser()
+
+    #"bert_classification/data/preprocessed"
+
+    # print(args.batch_size)
+
+    dm = PreprocessorClass(
+        preprocessed_dir = args.preprocessed_dir,
+        batch_size = args.batch_size,
+        max_length = args.max_length
+    )
+
+    model = MultiClassModel(
+        n_out = 5,
+        dropout = 0.3,  # dropout tentuin sendiri
+        lr = 1e-5   # 1e-3 = 0.0001
+    )
+
+    trainer = L.Trainer(
+        accelerator = args.accelerator,
+        max_epochs = args.max_epochs,
+        default_root_dir = 'logs/indobert'
+    )
+
+    # Ini bagian training 
+    trainer.fit(model, datamodule = dm)
+
+    # Testing model
+    trainer.test(datamodule = dm, ckpt_path = 'best')
+    # sama saja dengan trainer.test(datamodule = dm, ckpt_path = 'best')
+    # trainer.test(model=model, datamodule=dm, ckpt_path='best')
+    
+    # Prediction
+    # hasil = trainer.predict(model = model, datamodule = dm)
---
+++ utils/preprocessor_class.py
@@ -0,0 +1,259 @@
+import enum
+import pickle
+import torch
+import os
+import sys
+
+import re
+
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+import pytorch_lightning as pl
+import pandas as pd
+
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from transformers import BertTokenizer
+
+# untuk membuat progress bar
+from tqdm import tqdm 
+
+class PreprocessorClass(pl.LightningDataModule):
+    # 1. def __init__()
+    # 2. def setup()
+
+    def __init__(self,
+                 preprocessed_dir,
+                 batch_size = 10,
+                 max_length = 100,):
+
+        super(PreprocessorClass, self).__init__()
+
+        self.label2id = {
+            'semua usia': 0,
+            'anak': 1,
+            'remaja': 2,
+            'dewasa': 3
+        }
+
+        factory = StemmerFactory()
+        self.stemmer = factory.create_stemmer()
+
+        # Merubah kalimat menjadi id, tokenize dan attention
+        self.tokenizer = BertTokenizer.from_pretrained('indolem/indobert-base-uncased')
+
+        self.max_length = max_length
+        self.preprocessed_dir = preprocessed_dir
+
+        self.batch_size = batch_size
+
+
+    def clean_str(self, string):
+        string = string.lower()
+        string = re.sub(r"[^A-Za-z0-9(),!?\'\-`]", " ", string)
+        string = re.sub(r"\'s", " \'s", string)
+        string = re.sub(r"\'ve", " \'ve", string)
+        string = re.sub(r"n\'t", " n\'t", string)
+        # Menghapus enter
+        string = re.sub(r"\n", "", string)
+
+        # Membersihkan elemen yang tidak perlu, seperti menghapus spasi 2
+        string = re.sub(r"\'re", " \'re", string)
+
+        # Mengecek digit atau bukan
+        string = re.sub(r"\'d", " \'d", string)
+
+        # Mengecek long atau bukan
+        string = re.sub(r"\'ll", " \'ll", string)
+        string = re.sub(r",", " , ", string)
+        string = re.sub(r"!", " ! ", string)
+        string = re.sub(r"\(", " \( ", string)
+        string = re.sub(r"\)", " \) ", string)
+        string = re.sub(r"\?", " \? ", string)
+        string = re.sub(r"\s{2,}", " ", string)
+        string = string.strip()
+        # Menghilangkan imbuhan
+        return self.stemmer.stem(string)
+
+    def load_data(self,):
+        dataset = pd.read_excel("data/dataset_lyrics.xlsx")
+        dataset = dataset[["Title", "Lyric", "Age Class tag"]]
+        
+        # Mengetahui apa saja label yang ada di dalam dataset
+        label_yang_ada = dataset["Age Class tag"].drop_duplicates()
+        # print(label_yang_ada)
+        
+        # Konversi dari label text (semua usia) ke label id (0) dst
+        dataset["Age Class tag"] = dataset["Age Class tag"].map(self.label2id)
+        
+        # Mengetahui apa saja label setelah dikonversi
+        label_yang_ada = dataset["Age Class tag"].value_counts()
+        # print(label_yang_ada)
+
+        # sys.exit()
+        
+        return dataset
+    
+    def arrange_data(self, data):
+        # Yang di lakukan
+        # 1. Cleaning sentence
+        # 2. Tokenizing data
+        # 3. Arrange ke dataset (training, validation, testing)
+
+        # type untuk tipe datanya, apakah training atau testing
+
+        # y = label
+        x_input_ids, x_token_type_ids, x_attention_mask, y = [], [], [], []
+        for row, data in tqdm(data.iterrows(), total = data.shape[0], desc = "Preprocesing Song Lyrics"):
+            '''
+                'semua usia' = 0
+                'anak' = 1
+                'remaja' = 2
+                'dewasa' = 3
+            '''
+            
+            title = self.clean_str(data["Title"])
+            lyric = self.clean_str(data["Lyric"])
+            label = data["Age Class tag"]
+            
+            title = title.replace("lirik lagu ", "")
+
+            # Mengubah label yang tadinya angka menjadi binary
+            binary_lbl = [0] * len(self.label2id)
+            binary_lbl[label] = 1
+            
+            tkn = self.tokenizer(
+                f"{title} {lyric}", #batch_sentences
+                max_length = 200,
+                truncation = True,
+                padding = "max_length",
+            )
+            x_input_ids.append(tkn['input_ids'])
+            x_token_type_ids.append(tkn['token_type_ids'])
+            x_attention_mask.append(tkn['attention_mask'])
+            y.append(binary_lbl)
+
+            #saya suka makan ... ...
+            #input_ids = 3,1,5,0,0
+            #attention_mask = 1,1,1,0,0
+            
+            # anak2 = 0
+            # remaja = 1
+            # dewasa = 2
+            # semua umur = 3
+
+            # y dewasa
+            # y.append([0,0,1,0])
+
+            # if row == 4:
+            #     print("\n")
+            #     print("row = ", row)
+            #     print("label = ", label)
+            #     print("x = ", f"{title} {lyric}")
+            #     print("y = ", binary_lbl)
+            #     # sys.exit()
+            #     break
+
+        # Mengubah list ke tensor
+        x_input_ids = torch.tensor(x_input_ids)
+        x_token_type_ids = torch.tensor(x_token_type_ids)
+        x_attention_mask = torch.tensor(x_attention_mask)
+        y = torch.tensor(y)
+
+        tensor_dataset = TensorDataset(
+            x_input_ids,
+            x_token_type_ids,
+            x_attention_mask,
+            y
+        )
+
+        # Ratio Training 80% overall data = (90% Train, 10% Validation)
+        # Ratio Testing 20% overall data
+
+        train_val_len = int(x_input_ids.shape[0] * 0.8)   #100 * 0.8 = 80
+        train_len = int(train_val_len * 0.9)    #80 * 0.9 = 72
+        val_len = train_val_len - train_len #80 - 72 = 8
+        test_len = x_input_ids.shape[0] - train_val_len   #100 - 80 = 20
+
+        train_set, val_set, test_set = torch.utils.data.random_split(
+            tensor_dataset,
+            [train_len, val_len, test_len]
+        )
+
+        # print("\n")
+        # print("train_set = ", train_set)
+        # print("val_set = ", val_set)
+        # print("test_set = ", test_set)
+        # sys.exit()
+        
+        # f untuk merubah ke string
+        torch.save(train_set, f"{self.preprocessed_dir}/train.pt")
+        torch.save(val_set, f"{self.preprocessed_dir}/valid.pt")
+        torch.save(test_set, f"{self.preprocessed_dir}/test.pt")
+        
+        return train_set, val_set, test_set
+
+    def preprocessor(self,):
+        # membersihkan dan membuat tokenisasi
+        dataset = self.load_data()
+
+        # Mengecek apakah data train, valid, dan test sudah di preprocessed
+        if not os.path.exists(f"{self.preprocessed_dir}/train.pt") \
+            or not os.path.exists(f"{self.preprocessed_dir}/valid.pt") \
+            or not os.path.exists(f"{self.preprocessed_dir}/test.pt"):
+            train_data, valid_data, test_data = self.arrange_data(data = dataset)
+            print("Successfully created training, validation, and testing datasets")
+        else:
+            print("Load Preprocessed train and validation data data")
+            train_data = torch.load(f"{self.preprocessed_dir}/train.pt")
+            valid_data = torch.load(f"{self.preprocessed_dir}/valid.pt")
+            print("Load Preprocessed test data")
+            test_data = torch.load(f"{self.preprocessed_dir}/test.pt")
+
+        return train_data, valid_data, test_data
+
+    def setup(self, stage = None):
+        train_data, valid_data, test_data = self.preprocessor()
+        # print(valid_data)
+        # fit = training
+        # predict = testing
+        if stage == "fit":
+            self.train_data = train_data
+            self.valid_data = valid_data
+        elif stage == "predict":
+            self.test_data = test_data
+        
+    def train_dataloader(self):
+        sampler = RandomSampler(self.train_data)
+        return DataLoader(
+            dataset = self.train_data,
+            # Membagi process training dalam sekali proses
+            batch_size = self.batch_size, 
+            sampler = sampler,
+            num_workers = 4
+        )
+    
+    def val_dataloader(self):
+        sampler = SequentialSampler(self.valid_data)
+        return DataLoader(
+            dataset = self.valid_data,
+            # Membagi process training dalam sekali proses
+            batch_size = self.batch_size, 
+            sampler = sampler,
+            num_workers = 4
+        )
+    
+    def predict_dataloader(self):
+        sampler = SequentialSampler(self.test_data)
+        return DataLoader(
+            dataset = self.test_data,
+            # Membagi process training dalam sekali proses
+            batch_size = self.batch_size, 
+            sampler = sampler,
+            num_workers = 4
+        )
+
+# if __name__ == '__main__':
+#     Pre = PreprocessorClass(preprocessed_dir = "data/preprocessed")
+#     Pre.setup(stage = "fit")
+#     train_data = Pre.train_dataloader()
+#     print(train_data)
